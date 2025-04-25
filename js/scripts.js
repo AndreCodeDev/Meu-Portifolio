c801c453e3af913ebd8bb916a6eae652aef677dd
Andre Vitor <salesdias1207@gmail.com>
2025-04-25 14:46:50 -0300

    atualizações

--- js/scripts.js
+++ js/scripts.js
@@ -265,3 +265,65 @@ function initPortfolio() {
 function initAbout() {
     console.log('About inicializado');
 }
+
+
+
+
+
+
+
+
+
+const dots = document.querySelectorAll('.code-dot');
+let index = 0;
+
+function blinkNextDot() {
+  dots.forEach(dot => dot.classList.remove('active'));
+  dots[index].classList.add('active');
+
+  index = (index + 1) % dots.length;
+
+  setTimeout(blinkNextDot, 600);
+}
+
+// Start
+setTimeout(blinkNextDot, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+//carrocel about
+document.addEventListener("DOMContentLoaded", function () {
+    const carrossel = document.querySelector(".carrossel");
+    const carrosselItems = document.querySelectorAll(".carrossel-item");
+    const totalItems = carrosselItems.length;
+
+    // Clona os itens do carrossel para criar o efeito infinito
+    carrosselItems.forEach((item) => {
+        const clone = item.cloneNode(true);
+        carrossel.appendChild(clone);
+    });
+
+    let scrollPosition = 0;
+    const scrollSpeed = 1.5; // Velocidade do carrossel (ajuste conforme necessário)
+
+    function scrollCarrossel() {
+        scrollPosition += scrollSpeed;
+        if (scrollPosition >= carrossel.scrollWidth / 2) {
+            scrollPosition = 0; /* Reinicia a posição quando chegar ao final */
+        }
+        carrossel.style.transform = `translateX(-${scrollPosition}px)`;
+        requestAnimationFrame(scrollCarrossel);
+    }
+
+    // Inicia a animação
+    scrollCarrossel();
+});
