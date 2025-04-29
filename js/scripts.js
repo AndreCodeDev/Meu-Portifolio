0aea78908fc09b28a80b1f0b3d9bf20c3b9a72b2
Andre Vitor <salesdias1207@gmail.com>
2025-04-29 12:09:43 -0300

    responsive hero about

--- js/scripts.js
+++ js/scripts.js
@@ -326,4 +326,36 @@ setTimeout(lightRayEffect, 1000);
 
 
 
+//carrocel about
+document.addEventListener("DOMContentLoaded", function () {
+    const carrossel = document.querySelector(".carrossel");
+    const carrosselItems = document.querySelectorAll(".carrossel-item");
+    const totalItems = carrosselItems.length;
+
+    // Clona os itens do carrossel para criar o efeito infinito
+    carrosselItems.forEach((item) => {
+        const clone = item.cloneNode(true);
+        carrossel.appendChild(clone);
+    });
+
+    let scrollPosition = 0;
+    const scrollSpeed = 1.5; // Velocidade do carrossel (ajuste conforme necessário)
+
+    function scrollCarrossel() {
+        scrollPosition += scrollSpeed;
+        if (scrollPosition >= carrossel.scrollWidth / 2) {
+            scrollPosition = 0; /* Reinicia a posição quando chegar ao final */
+        }
+        carrossel.style.transform = `translateX(-${scrollPosition}px)`;
+        requestAnimationFrame(scrollCarrossel);
+    }
+
+    // Inicia a animação
+    scrollCarrossel();
+});
+
+
+
+
+
 
